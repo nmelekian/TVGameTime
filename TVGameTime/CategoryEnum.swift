fb65e18a5c632a0b569b17653bcdf750fffc2ce4
Nick Melekian <nick.melekian@gmail.com>
2023-09-13 16:55:02 -0400

    added category and question models

---
+++ TVGameTime/CategoryEnum.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryEnum.swift
+//  TVGameTime
+//
+//  Created by Nick Melekian on 9/13/23.
+//
+
+import Foundation
---
+++ TVGameTime/QuestionModel.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionModel.swift
+//  TVGameTime
+//
+//  Created by Nick Melekian on 9/13/23.
+//
+
+import Foundation
