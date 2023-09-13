afcc33640aaf8cca876e90340843a5afb132d640
Nick Melekian <nick.melekian@gmail.com>
2023-09-13 15:39:47 -0400

    Initial Commit

---
+++ TVGameTime/TVGameTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVGameTimeApp.swift
+//  TVGameTime
+//
+//  Created by Nick Melekian on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct TVGameTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
