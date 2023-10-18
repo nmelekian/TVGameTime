e32b3350ff97e047809c61d4803d811b97f24c48
Nick Melekian <nick.melekian@gmail.com>
2023-10-18 10:39:42 -0400

    hi

--- TVGameTime/QuestionViewModel.swift
+++ TVGameTime/QuestionViewModel.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-
+@MainActor
 class QuestionViewModel: ObservableObject {
-//    @Published var questions: [Question] = [Question(category: .history, question: "What is the name of the first president", correctAnswer: "George Washington", answers: ["Abraham Lincoln", "Herbert Hoover", "George Washington", "Milard Filmore"]), Question(category: .math, question: "What is 2 + 2?", correctAnswer: "4", answers: ["4", "1", "3", "2"])]
+//    @Published var questions: [Question] = [Question(category: .history, question: "What is the name of the first president", correctAnswer: "George Washington", answers: ["Abraham Lincoln", "Herbert Hoover", "George Washington", "Milard Filmore"]), Question(category: .math, question: "What is 2 + 2?", correctAnswer: "4", answers: ["4", "1", "3", "2"])]ojkhklhliklih
     
     @Published var questions: [Question] = []
     @Published var score = 0
@@ -28,6 +28,7 @@ class QuestionViewModel: ObservableObject {
         self.isLoading = true
         do {
             let data = try await service.fetchQuestion(category: category)
+                
             self.questions = data
             self.isLoading = false
         } catch {
